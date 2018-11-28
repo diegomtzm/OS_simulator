5a6cb24b822791305c556f1d4018ee0c6f338972
Diego Martinez <diegomtz1998@hotmail.com>
2018-11-27 22:26:13 -0600

    Receive and process corresponding command

--- servidor.py
+++ servidor.py
@@ -28,28 +28,78 @@ connection, client_address = sock.accept()
 # accept() returns an open connection between the server and client, along with the address of the client.
 # The connection is actually a different socket on another port (assigned by the kernel). Data is read from the connection with recv() and transmitted with sendall().
 
+def create(*p):
+	params = p[0]
+	print (params)
+	connection.send("parametros: " + ', '.join(params))
+	return "funcion create"
+
+def address(*p):
+	params = p[0]
+	print (params)
+	connection.send("parametros: " + ', '.join(params))
+	return "funcion address"
+
+def create_priority(*p):
+	params = p[0]
+	print (params)
+	connection.send("parametros: " + ', '.join(params))
+	return "funcion createp"
+
+def quantum(*p):
+	params = p[0]
+	print (params)
+	connection.send("parametros: " + ', '.join(params))
+	return "funcion quantum"
+
+def termina_proceso(*p):
+	params = p[0]
+	print (params)
+	connection.send("parametros: " + ', '.join(params))
+	return "funcion fin"
+
+def end_simulation(*p):
+	print ("[end]")
+	connection.send("simulacion terminada")
+	return "funcion end"
+
+def invalid(*p):
+	print ("Comando invalido")
+	connection.send("comando invalido")
+	return "funcion invalid"
+
+
 try:
 	print >>sys.stderr, 'connection from', client_address
 
     # Receive the data 
 	while True:   
-		data = connection.recv(256)
-		print >>sys.stderr, 'server received "%s"' % data
+		msg = connection.recv(256)
+		data = msg.split()
+
+		comando = {
+			'Create': create,
+			'Address': address,
+			'CreateP': create_priority,
+			'Quantum': quantum,
+			'Fin': termina_proceso,
+			'End': end_simulation,
+		}.get(data[0], invalid)(data[1:])
+
 		if data:
-			print >>sys.stderr, 'sending answer back to the client'
-	
-			connection.sendall('mensaje recibido!')
+			print >>sys.stderr, 'server received "%s"' % comando
 		else:
 			print >>sys.stderr, 'no data from', client_address
 			connection.close()
 			sys.exit()
-			
+
 finally:
      # Clean up the connection
 	print >>sys.stderr, 'se fue al finally'
 	connection.close()
 
-#When communication with a client is finished, the connection needs to be cleaned up using close(). This example uses a try:finally block to ensure that close() is always called, even in the event of an error.
+# When communication with a client is finished, the connection needs to be cleaned up using close(). 
+# This example uses a try:finally block to ensure that close() is always called, even in the event of an error.
 
 def main(args):
     return 0
